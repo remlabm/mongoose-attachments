9050409383b14a89aa9e844d1861c68ea3417e77
Nathan Kot <me@nathankot.com>
2014-11-03 08:15:01 +0900

    Bootstrap - make it work with watch.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,2 +1,56 @@
 global.chai = require('chai');
 global.expect = chai.expect;
+
+var fs = require('fs');
+var mongoose = require('mongoose');
+var plugin = require('../lib/attachments');
+
+if (!plugin.providersRegistry.fakeProvider) {
+
+  var fakeProvider = function(){};
+
+  fakeProvider.prototype.getUrl = function(path){
+    return path;
+  };
+
+  fakeProvider.prototype.createOrReplace = function(attachment, next){
+    attachment.defaultUrl = this.getUrl(attachment.path);
+
+    console.log(attachment.filename);
+    fs.createReadStream(attachment.filename).pipe(
+      fs.createWriteStream(attachment.path)
+      .on('finish', function() {
+        next(null, attachment);
+      })
+    );
+  };
+
+  plugin.registerStorageProvider('fakeProvider', fakeProvider);
+
+}
+
+if (!mongoose.models.User) {
+
+  UserSchema = new mongoose.Schema({ });
+
+  UserSchema.plugin(plugin, {
+    directory: process.cwd() + '/tmp',
+    storage: { providerName: 'fakeProvider', options: { } },
+    properties: {
+      profile: { styles: { original: { } } },
+      avatar:  {
+        styles: {
+          original: { },
+          thumbnail: {
+            thumbnail: '140x140^',
+            gravity: 'center',
+          }
+        }
+      },
+    }
+  });
+
+  mongoose.model('User', UserSchema);
+
+}
+
