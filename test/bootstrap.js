99c53edef4a4c382c00a8f7ecb76e4edc7093ed0
Nathan Kot <me@nathankot.com>
2014-11-03 08:24:22 +0900

    Thumbnail should be smaller.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -43,7 +43,7 @@ if (!mongoose.models.User) {
         styles: {
           original: { },
           thumbnail: {
-            thumbnail: '140x140^',
+            thumbnail: '25x25^',
             gravity: 'center',
           }
         }
