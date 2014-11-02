2d27ca91e8dfc0af2d7647d6270aa6712634b76d
Nathan Kot <me@nathankot.com>
2014-11-02 12:18:32 +0900

    Formatting.

--- test/defaultUrl.spec.js
+++ test/defaultUrl.spec.js
@@ -25,16 +25,18 @@ UserSchema.plugin(plugin, {
 var User = mongoose.model('User', UserSchema);
 
 describe('path', function(){
+
   it('adds the propertyName in the attached image path', function(done){
     var user = new User({});
     var path = { path: process.cwd() + '/test/fixtures/mongodb.png' };
-    user.attach('profile', path, function(err){
-      user.attach('avatar', path, function(err){
+    user.attach('profile', path, function(err) {
+      user.attach('avatar', path, function(err) {
         expect(user.avatar.original.defaultUrl).to.not.eql(user.profile.original.defaultUrl);
         expect(user.avatar.original.defaultUrl).to.include('users/avatar/' + user.id + '-original.png');
         expect(user.profile.original.defaultUrl).to.include('users/profile/' + user.id + '-original.png');
         done();
-      })
+      });
     });
   });
+
 });
